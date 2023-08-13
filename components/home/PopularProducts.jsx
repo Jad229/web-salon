4902002410fca4e627a575c399de3cbba8e4f657
Jad229 <jad229@njit.edu>
2023-08-13 16:15:15 -0400

    Home page testimonial finished

---
+++ components/home/PopularProducts.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PopularProducts() {
+  return (
+    <div>
+      <h1 className="subheading_text">Popular Products</h1>
+    </div>
+  );
+}
+
+export default PopularProducts;
