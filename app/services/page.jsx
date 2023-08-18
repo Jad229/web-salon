eda6f5e588611ff46c6309ef626309bb2242a012
Jad229 <76543236+Jad229@users.noreply.github.com>
2023-08-18 18:04:36 -0400

    issue: mapping of servicesData is returning empty

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+"use client";
+import ServicesList from "@components/services/ServicesList";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Services() {
-  return <div>Services</div>;
+  const [servicesData, setServicesData] = useState([]);
+
+  useEffect(() => {
+    const getServices = async () => {
+      try {
+        const response = await axios.get("/api/services");
+        setServicesData(response.data);
+      } catch (error) {
+        console.error("Error fetching services:", error);
+      }
+    };
+
+    getServices();
+  }, []);
+
+  console.log(servicesData);
+
+  return (
+    <section>
+      <div>
+        <h1>Services</h1>
+      </div>
+      <div>
+        <div>
+          <ServicesList services={servicesData.hair} name="hair" />
+        </div>
+        <div></div>
+      </div>
+    </section>
+  );
 }
 
 export default Services;
